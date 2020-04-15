4c11a5ad98926038ceb793e6d36943b413296ab8
skrall3119 <alexjanci@yahoo.com>
2020-04-15 13:48:14 -0500

    Initial Commit

---
+++ RestaurantPOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantPOS
+//
+//  Created by Alex Janci on 4/15/20.
+//  Copyright © 2020 Alex Janci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
